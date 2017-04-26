48b3f6f3e616f3fabb144d415c18bf4ca35882ca
Erik Iverson <erik@sigmafield.org>
2017-04-25 17:11:29 -0700

    add react pose collection function

---
+++ index.js
@@ -0,0 +1,23 @@
+import vrtigo from 'vrtigo-analytics';
+import { VrHeadModel } from 'react-vr';
+
+const collectPose = function() {
+  return VrHeadModel.rotationOfHeadMatrix();
+};
+
+vrtigo.setPoseFunction(collectPose);
+
+export default {
+  start: vrtigo.start,
+  stop: vrtigo.stop,
+  seekBegin: vrtigo.seekBegin,
+  seekEnd: vrtigo.seekEnd,
+  pause: vrtigo.pause,
+  unpause: vrtigo.unpause,
+  bufferBegin: vrtigo.bufferBegin,
+  bufferEnd: vrtigo.bufferEnd,
+  submit: vrtigo.submit,
+  setUserId: vrtigo.setUserId,
+  setAppId: vrtigo.setAppId,
+  submit: vrtigo.submit
+};
